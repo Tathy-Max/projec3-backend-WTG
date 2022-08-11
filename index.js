c7b744e115c431971a58874e8f9ec1048377def6
Alexandre Lima <81311412+alexandreatlima@users.noreply.github.com>
2022-08-11 15:54:40 -0300

    att CORS

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const app = express();
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-	req.header(
-		'Access-Control-Allow-Origin',
-		'https://project-three-ironhack.netlify.app'
-	);
-	req.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	req.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-	next();
-});
+// app.use(function (req, res, next) {
+// 	req.header(
+// 		'Access-Control-Allow-Origin',
+// 		'https://project-three-ironhack.netlify.app'
+// 	);
+// 	req.header(
+// 		'Access-Control-Allow-Headers',
+// 		'Origin, X-Requested-With, Content-Type, Accept'
+// 	);
+// 	req.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+// 	next();
+// });
 
-// app.use(cors({ origin: process.env.REACT_APP_URL }));
+app.use(cors({ origin: process.env.REACT_APP_URL }));
 
 const uploadImgRouter = require('./routes/uploadimg.routes');
 app.use('/', uploadImgRouter);
